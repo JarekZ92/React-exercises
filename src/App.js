7d6db5a2b8f366ca670c83907d0307441afc7fbf
Jarek <z.jarek92@gmail.com>
2018-08-10 14:24:09 +0200

    added method to post data on firebase

--- src/App.js
+++ src/App.js
@@ -23,18 +23,30 @@ class Counter extends React.Component {
     })
   }
 
-  // https://react-exercises.firebaseio.com/
-
-    render() {
-      return (
-        <div>
-          <h1>{this.state.startCount}</h1>
-          <button onClick={this.additionCounter}> + </button>
-          <button onClick={this.substractCounter}> - </button>
-          <br /> <button onClick={this.resetCounter}> Reset counter </button>
-        </div>
-      );
+  componentDidMount() {
+    const request = {
+      method: "POST",
+      body: JSON.stringify({ startCount: this.state.startCount })
     }
+
+    fetch('https://react-exercises.firebaseio.com/counter/state.json', request)
+      .then(response => {
+        this.setState({
+          startCount: this.state.startCount
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.startCount}</h1>
+        <button onClick={this.additionCounter}> + </button>
+        <button onClick={this.substractCounter}> - </button>
+        <br /> <button onClick={this.resetCounter}> Reset counter </button>
+      </div>
+    );
+  }
 }
 
 export default Counter;
